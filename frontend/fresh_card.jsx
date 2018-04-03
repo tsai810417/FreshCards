fd87eb9e1718868d8dee2c05348bdc0553dbc35f
tsai810417 <tsai_cheng@yahoo.com>
2018-04-03 14:10:59 -0700

    Finish session_actions and fix errors in entry file

--- frontend/fresh_card.jsx
+++ frontend/fresh_card.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
   ReactDOM.render(<h1>Welcome to FreshCard</h1>, root);
 });
