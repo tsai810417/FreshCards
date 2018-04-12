6cd73b229a474bc7feafbdbf67150b1f6bac89bb
tsai810417 <tsai_cheng@yahoo.com>
2018-04-11 22:41:59 -0700

    shrink size of image

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -5,7 +5,7 @@ class Landing extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='landing-container'>
         <div className='img-container'>
           <img src='/assets/landing_img.jpg' />
           <p>This is a cool site</p>
